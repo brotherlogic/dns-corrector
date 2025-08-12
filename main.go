a621aaf5476170bb5b8ef1a36a892ea92dbb6acb
brotherlogic <brotherlogic@gmail.com>
2025-08-12 12:28:21 -0700

    Added first pass at IP resolution

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "log"
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+func getExternalIP(ctx context.Context) (string, error) {
+	res, err := http.Get("wtfismyip.com/text")
+	if err != nil {
+		return "", fmt.Errorf("unable to get data: %w", err)
+	}
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", fmt.Errorf("unable to read body: %w", err)
+	}
+
+	return string(resBody), nil
+}
 
 func main() {
 	log.Printf("First Pass")
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	ip, err := getExternalIP(ctx)
+	log.Printf("Got IP: %v, %v", ip, err)
 }
