9dbd88cdaa32c14c4ac883af9422fe7d9c977917
brotherlogic <brotherlogic@gmail.com>
2025-08-25 06:19:46 -0700

    Working

--- main.go
+++ main.go
@@ -8,7 +8,22 @@ import (
 	"net"
 	"net/http"
 	"time"
+
+	"github.com/cloudflare/cloudflare-go/v5"
+	"github.com/cloudflare/cloudflare-go/v5/dns"
+	"github.com/cloudflare/cloudflare-go/v5/option"
 )
+
+type server struct {
+	client *cloudflare.Client
+}
+
+func (s *server) runCorrection(ctx context.Context, newIP string) error {
+	s.client.DNS.Records.Update(ctx, "monitoring", dns.RecordUpdateParams{
+		ZoneID: cloudflare.F("brotherlogic-backend.com"),
+	})
+	return nil
+}
 
 func getExternalIP(ctx context.Context) (string, error) {
 	res, err := http.Get("http://wtfismyip.com/text")
@@ -37,19 +52,35 @@ func resolveIP(ctx context.Context, address string) (string, error) {
 	return ips[0].To16().String(), nil
 }
 
-func main() {
+func (s *server) loop() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	ip, err := getExternalIP(ctx)
+	log.Printf("Got IP: %v, %v", ip, err)
+
+	ip, err = resolveIP(ctx, "gramophile-grpc.brotherlogic-backend.com")
+	log.Printf("Resolved: %v and %v", ip, err)
+}
+
+func (s *server) run() {
+
 	for {
-		log.Printf("First Pass")
-
-		ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-		defer cancel()
-
-		ip, err := getExternalIP(ctx)
-		log.Printf("Got IP: %v, %v", ip, err)
-
-		ip, err = resolveIP(ctx, "gramophile-grpc.brotherlogic-backend.com")
-		log.Printf("Resolved: %v and %v", ip, err)
-
+		s.loop()
 		time.Sleep(time.Minute)
 	}
+}
+
+func main() {
+
+	client := cloudflare.NewClient(
+		option.WithAPIToken("TOKEN"),
+	)
+
+	s := &server{
+		client: client,
+	}
+
+	s.run()
+
 }
